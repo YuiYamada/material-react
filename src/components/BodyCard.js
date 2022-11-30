df90fe92d79d4578ec0ae8104f31a17ebbcc3de2
yama <ystv1212@gmail.com>
2022-12-01 06:45:47 +0900

    テスト用APIでリスト表示完了

--- src/components/BodyCard.js
+++ src/components/BodyCard.js
@@ -23,10 +23,23 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  cHeader: {
+    height: "50px",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    "& .MuiCardHeader-content": {
+      overflow: "hidden",
+    },
+  },
+  cContent: {
+    height: "200px",
+    overflow: "hidden",
+  },
 });
 
 function BodyCard(props) {
-  const { avatarUrl, title, subheader, text, imageUrl } = props;
+  const { userId, id, title, body, avatarUrl, imageUrl } = props;
   const classes = useStyles();
   const bull = <span className={classes.bullet}>•</span>;
   return (
@@ -38,13 +51,13 @@ function BodyCard(props) {
             <StarBorderOutlinedIcon />
           </IconButton>
         }
+        className={classes.cHeader}
         title={title}
-        subheader={subheader}
       />
       <CardMedia style={{ height: "150px" }} image={imageUrl} />
-      <CardContent>
+      <CardContent className={classes.cContent}>
         <Typography variant="body2" component="p">
-          {text}
+          {body}
         </Typography>
       </CardContent>
       <CardActions>
